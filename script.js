2585b77242a687b4c95c265c059a9132c059e4b7
jnsvrschr <106373661+jnsvrschr@users.noreply.github.com>
2022-08-07 11:39:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const accuracy = position.coords.accuracy;
+    const altitude = position.coords.altitude;
+    const altitudeAccuracy = position.coords.altitudeAccuracy;
+    const heading = position.coords.heading;
+    const speed = position.coords.speed;
+    const timestamp = position.timestamp;
+    document.querySelector('h1').innerHTML = latitude + '<br>' + longitude + '<br>' + accuracy + '<br>'
+    + altitude + '<br>' + altitudeAccuracy + '<br>' + heading + '<br>' + speed + '<br>' + timestamp;
+}
+
+function error() {
+    document.querySelector('h1').textContent = 'locatie niet gevonden';
+}
+
+const options = {
+    enableHighAccuracy: true
+};
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+} else {
+    alert('geolocatie is niet ondersteund door jouw browser');
+}
